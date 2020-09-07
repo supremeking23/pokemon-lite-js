58cb46be2e9051400dec51d9a109f321e1d4896b
Ivan Christian Jay Funcion <icjfuncion@gmail.com>
2020-09-08 01:51:05 +0800

    add additional design

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -50,6 +50,9 @@ async function getAllPokemon() {
       } else if (type1 == "flying") {
         bg = "#8899FF";
         bgtype1 = "#66CCFF";
+      } else if (type1 == "poison") {
+        bg = "#8D4690";
+        bgtype1 = "#EE99EE";
       } else if (type1 == "bug") {
         bg = " #AABB22";
         bgtype1 = "#8CD750";
@@ -57,9 +60,41 @@ async function getAllPokemon() {
         bg = "#BFBFBF";
         bgtype1 = "#B7B6A4";
       } else if (type1 == "dark") {
-        bg = "#8C6F61";
+        bg = "#352E27";
         bgtype1 = "#8C6F61";
+      } else if (type1 == "psychic") {
+        bg = "#D24F7B";
+        bgtype1 = "#FF5599";
+      } else if (type1 == "electric") {
+        bg = "#FEB820";
+        bgtype1 = "#FFDD57";
+      } else if (type1 == "rock") {
+        bg = "#A29045";
+        bgtype1 = "#BBAA66";
+      } else if (type1 == "ground") {
+        bg = " #C8A955";
+        bgtype1 = "#DDBB55";
+      } else if (type1 == "fighting") {
+        bg = "#813418";
+        bgtype1 = "#C56F61";
+      } else if (type1 == "ghost") {
+        bg = "#4D4D8C";
+        bgtype1 = "#7D7DC5";
+      } else if (type1 == "ice") {
+        bg = "#6ACFE9";
+        bgtype1 = "#7DD4FF";
+      } else if (type1 == "steel") {
+        bg = "#8E8CA0";
+        bgtype1 = "#B7B7C5";
+      } else if (type1 == "dragon") {
+        bg = "#685BB0";
+        bgtype1 = "#8C7DF1";
+      } else if (type1 == "fairy") {
+        bg = "#E09EDA";
+        bgtype1 = "#F1A9F1";
       }
+
+      //type 2
 
       if (type2 == "fire") {
         bgtype2 = "#FF6144";
@@ -71,6 +106,32 @@ async function getAllPokemon() {
         bgtype2 = "#8899FF";
       } else if (type2 == "poison") {
         bgtype2 = "#EE99EE";
+      } else if (type2 == "bug") {
+        bgtype2 = "#8CD750";
+      } else if (type2 == "normal") {
+        bgtype2 = "#B7B6A4";
+      } else if (type2 == "dark") {
+        bgtype2 = "#8C6F61";
+      } else if (type2 == "psychic") {
+        bgtype2 = "#FF5599";
+      } else if (type2 == "electric") {
+        bgtype2 = "#FFDD57";
+      } else if (type2 == "rock") {
+        bgtype2 = "#BBAA66";
+      } else if (type2 == "ground") {
+        bgtype2 = "#DDBB55";
+      } else if (type2 == "fighting") {
+        bgtype2 = "#C56F61";
+      } else if (type2 == "ghost") {
+        bgtype2 = "#7D7DC5";
+      } else if (type2 == "ice") {
+        bgtype2 = "#7DD4FF";
+      } else if (type2 == "steel") {
+        bgtype2 = "#B7B7C5";
+      } else if (type2 == "dragon") {
+        bgtype2 = "#8C7DF1";
+      } else if (type2 == "fairy") {
+        bgtype2 = "#F1A9F1";
       }
 
       let data = `
@@ -79,7 +140,9 @@ async function getAllPokemon() {
               <div id="poke-container" class="d-flex flex-column align-items-center justify-content-center pokemon-img-container " style="background:${bg}">
     
               <img src="${image}"
-              class="card-img-top pokemon-img" alt="..." />
+              class="card-img-top pokemon-img" alt="${capitalize(
+                pokemon.name
+              )}" />
               <div class="d-flex ${
                 type2 ? `justify-content-between` : `justify-content-center`
               } type-container">
